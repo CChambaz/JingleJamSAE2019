4b694b1ec1ff9eb135559a801ca6b3a6d3f7cabb
Sandwish <dylan_vonarx@hotmail.fr>
2019-12-16 20:31:21 +0100

    Added Enum TypeGame and repair scene
    MAIN_MENU = 0
    IN_GAME_1 = 1
    IN_GAME_2 = 2
    IN_GAME_3 = 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Canvas View_1;
+    [SerializeField] private Canvas View_2;
+    [SerializeField] private Canvas View_3;
+
+    private Canvas canvasSelect;
+
+    public enum TypeGame
+    {
+        MAIN_MENU,
+        IN_GAME_1,
+        IN_GAME_2,
+        IN_GAME_3,
+        IN_PAUSE
+    }
+
+    private TypeGame type = TypeGame.IN_GAME_1;
+
     public const int SNOWBALL_TYPE_COUNT = 4;
 
     enum SnowballType
@@ -14,8 +31,22 @@ public class GameManager : MonoBehaviour
         FLAKE
     }
 
+    public void Start()
+    {
+
+        canvasSelect = View_1;
+
+        SetTypeGame((int)type);
+    }
+
+    void ChangeCanavasSelect()
+    {
+
+
+    }
+
     public static GameManager Instance;
-    
+
     void Awake()
     {
         Instance = this;
@@ -40,6 +71,31 @@ public class GameManager : MonoBehaviour
     {
         get => moneyAmount;
         set => moneyAmount = value;
+    }
+
+    public void SetTypeGame(int type)
+    {
+        switch (type)
+        {
+            case (int)TypeGame.IN_GAME_1:
+                this.type = TypeGame.IN_GAME_1;
+                View_1.gameObject.SetActive(true);
+                View_2.gameObject.SetActive(false);
+                View_3.gameObject.SetActive(false);
+                break;
+            case (int)TypeGame.IN_GAME_2:
+                this.type = TypeGame.IN_GAME_2;
+                View_1.gameObject.SetActive(false);
+                View_2.gameObject.SetActive(true);
+                View_3.gameObject.SetActive(false);
+                break;
+            case (int)TypeGame.IN_GAME_3:
+                this.type = TypeGame.IN_GAME_3;
+                View_1.gameObject.SetActive(false);
+                View_2.gameObject.SetActive(false);
+                View_3.gameObject.SetActive(true);
+                break;
+        }
     }
 
     [SerializeField] private Canvas menu;
