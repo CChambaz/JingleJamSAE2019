7b9aa2fd2806d4adc5f031ef9285a91163d6be7b
Unknown <lucafloreau@akeonet.com>
2019-12-16 16:37:07 +0100

    selector + production

---
+++ Assets/Scripts/Mold/MoldManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoldManager : MonoBehaviour
+{
+    [SerializeField] private MoldSO[] molds;
+    public MoldSO[] Molds => molds;
+
+    private MoldSO currentMold;
+    public MoldSO CurrentMold => currentMold;
+
+    private MoldProduction moldProduction;
+
+    private const int snowballTypeCount = 4;
+
+    private int selectedMold = 0;
+    public int SelectedMold
+    {
+        get => selectedMold;
+        set
+        {
+            selectedMold = value;
+            selectedMold = selectedMold % (snowballTypeCount);
+            if (selectedMold < 0)
+            {
+                selectedMold = snowballTypeCount - 1;
+            }
+            currentMold = molds[selectedMold];
+            moldProduction.OnMoldChange();
+        }
+    }
+
+    void Awake()
+    {
+        currentMold = molds[selectedMold];
+    }
+
+    void Start()
+    {
+        moldProduction = FindObjectOfType<MoldProduction>();
+    }
+
+    public void UpdradeSpeed(float newSpeed)
+    {
+        molds[selectedMold].TimerSpeed += newSpeed;
+    }
+}
---
+++ Assets/Scripts/Mold/ScriptableObject/MoldSO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class MoldSO : ScriptableObject
+{
+    [SerializeField] private float maxTimer;
+    public float MaxTimer
+    {
+        get => maxTimer;
+        set => maxTimer = value;
+    }
+    [SerializeField] private float timerSpeed;
+    public float TimerSpeed
+    {
+        get => timerSpeed;
+        set => timerSpeed = value;
+    }
+    [SerializeField] private int ballRate;
+    public int BallRate
+    {
+        get => ballRate;
+        set => ballRate = value;
+    }
+    [SerializeField] private int snowCost;
+    public int SnowCost
+    {
+        get => snowCost;
+        set => snowCost = value;
+    }
+}
