343c76ba8e810a5df0ee2c3a71c5e47c7cbfd555
CChambaz <cdricchambaz@yahoo.fr>
2019-12-17 19:50:15 +0100

    Added taxes script

---
+++ Assets/Scripts/Charge/Charge.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Charge : MonoBehaviour
+{
+    [SerializeField] private TMP_Text taxeText;
+    [SerializeField] private TMP_Text maintainText;
+    
+    [SerializeField] private int taxesSlaveCost;
+    [SerializeField] private int taxesInstallationCost;
+    [SerializeField] private float taxesOnAnnualRevenu;
+    [SerializeField] private int installationMaintenance;
+    
+    private Slaves slaves;
+
+    private int automationCount = 0;
+    public int AutomationCount
+    {
+        get => automationCount;
+        set => automationCount = value;
+    }
+    
+    private int annualRevenu = 0;
+    public int AnnualRevenu
+    {
+        get => annualRevenu;
+        set => annualRevenu = value;
+    }
+
+    private void Start()
+    {
+        slaves = FindObjectOfType<Slaves>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maintainText.text = (installationMaintenance * automationCount).ToString();
+        taxeText.text = (taxesSlaveCost * slaves.AmountSlaves + taxesInstallationCost * automationCount).ToString();
+    }
+
+    public void ApplyMaintenanceCost()
+    {
+        GameManager.Instance.MoneyAmount -= installationMaintenance * automationCount;
+    }
+
+    public void ApplyTaxes()
+    {
+        GameManager.Instance.MoneyAmount -= taxesSlaveCost * slaves.AmountSlaves + taxesInstallationCost * automationCount + (int)((float)annualRevenu * taxesOnAnnualRevenu);
+        annualRevenu = 0;
+        
+    }
+}
